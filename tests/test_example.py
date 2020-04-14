2a7a579f6ff6554c96b69fdd61043e171605105f
Ariane Mora <ariane.mora@uq.net.au>
2020-04-14 14:33:28 +1000

    Added base example.

---
+++ tests/test_example.py
@@ -0,0 +1,41 @@
+###############################################################################
+#                                                                             #
+#    This program is free software: you can redistribute it and/or modify     #
+#    it under the terms of the GNU General Public License as published by     #
+#    the Free Software Foundation, either version 3 of the License, or        #
+#    (at your option) any later version.                                      #
+#                                                                             #
+#    This program is distributed in the hope that it will be useful,          #
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of           #
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the            #
+#    GNU General Public License for more details.                             #
+#                                                                             #
+#    You should have received a copy of the GNU General Public License        #
+#    along with this program. If not, see <http://www.gnu.org/licenses/>.     #
+#                                                                             #
+###############################################################################
+
+import os
+import shutil
+import tempfile
+import unittest
+
+from EXAMPLE_PROJECT import Example
+
+class TestExample(unittest.TestCase):
+
+    def setUp(self):
+        # Flag to set data to be local so we don't have to download them repeatedly. ToDo: Remove when publishing.
+        self.local = True
+
+        if self.local:
+            THIS_DIR = os.path.dirname(os.path.abspath(__file__))
+            self.tmp_dir = os.path.join(THIS_DIR, 'data/tmp/')
+            if os.path.exists(self.tmp_dir):
+                shutil.rmtree(self.tmp_dir)
+            os.mkdir(self.tmp_dir)
+        else:
+            self.tmp_dir = tempfile.mkdtemp(prefix='EXAMPLE_PROJECT_tmp_')
+
+    def tearDown(self):
+        shutil.rmtree(self.tmp_dir)
